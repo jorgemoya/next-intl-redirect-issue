31181960a94dcaf15817e719b87d5fdacb58a886
Jorge Moya <jorge.moya@bigcommerce.com>
2024-10-21 12:17:25 -0500

    Issue with escaped ? characters

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,6 +1,20 @@
-import {useTranslations} from 'next-intl';
+import { useTranslations } from "next-intl";
+import Link from "next/link";
+import { Link as NextIntlLink } from "@/src/i18n/routing";
 
 export default function IndexPage() {
-  const t = useTranslations('IndexPage');
-  return <h1>{t('title')}</h1>;
+  const t = useTranslations("IndexPage");
+  return (
+    <div>
+      <h1>{t("title")}</h1>
+      <div>
+        <Link href="/?foo=Hello&bar=World">Next Link with params</Link>
+      </div>
+      <div>
+        <NextIntlLink href="/?foo=Hello&bar=World">
+          Next Intl Link with params
+        </NextIntlLink>
+      </div>
+    </div>
+  );
 }
